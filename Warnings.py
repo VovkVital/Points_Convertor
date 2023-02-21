9bceffd3cfc4262a12902b35faa2ba3e53934c61
Vitalii Vovk <Vovk.Vitaliiy@gmail.com>
2023-02-21 15:23:08 -0800

    Added class Warning

--- Warnings.py
+++ Warnings.py
@@ -20,15 +20,15 @@ TAB_NAME = ["Design Folders", "Config Files", "Machine Files"]
 
 
 
-class Message_box(tkinter.ttk.Treeview):
+class Error_message(tkinter.ttk.Treeview):
 
-    def __init__(self, data, message, **kwargs):
+    def __init__(self, message, time, **kwargs):
         self.frame()
         self.flash=Button.flash
-        super(). __init__(master=self.message_frame)
-        self.data = data
+        super(). __init__(master=self.box)
         self.buttons(message=message)
-        self.close_frame(time=6)
+        self.close_frame()
+
 
     def frame(self):
         self.box = customtkinter.CTkToplevel()
@@ -36,27 +36,51 @@ class Message_box(tkinter.ttk.Treeview):
         self.box.minsize(width=500, height=250)
         self.box.maxsize(width=500, height=250)
         self.box.title("Error")
-        self.box.rowconfigure(0, weight=1, minsize=60)
-        self.box.rowconfigure(1, weight=2)
-        self.box.rowconfigure(2, minsize=10)
-        self.box.rowconfigure(3, weight=1, minsize=40)
-        self.box.rowconfigure(4, minsize=15)
+        self.box.rowconfigure(0, weight=1)
+        self.box.rowconfigure(1, minsize=60)
+        self.box.rowconfigure(2, minsize=20)
         self.box.columnconfigure(0, weight=1)
-        self.box.columnconfigure(1, weight=1)
-        self.message_frame = customtkinter.CTkFrame(master=self.box)
-        self.message_frame.grid(row=1, column=0, columnspan=2, sticky="news")
-        self.message_frame.rowconfigure(0, weight=1)
-        self.message_frame.columnconfigure(0, weight=1)
+
 
     def buttons(self, message):
-        self.button_accept = Button(master=self.box, text="Accept", sticky=None, row=3, column=0)
+        self.button_accept = Button(master=self.box, text="Accept", sticky=None, row=1, column=0,
+                                    command=self.box.destroy)
         label = Label(master=self.box, text=f"{message}", row=0, column=0)
-        label.grid(columnspan=2)
         label.configure(text_color=SELECTED_BLUE, font=FONT_LABEL_ERROR)
 
-    def close_frame(self, time=800):
+    def close_frame(self, time=8000):
         self.after(time, lambda: self.box.destroy())
 
+
+class Exception_message(tkinter.ttk.Treeview):
+    def __init__(self, message, time, **kwargs):
+        self.frame()
+        self.flash = Button.flash
+        super().__init__(master=self.box)
+        self.buttons()
+        self.text_box(message=message)
+        self.close_frame()
+    def frame(self):
+        self.box = customtkinter.CTkToplevel()
+        self.box.geometry("500x250")
+        self.box.minsize(width=500, height=250)
+        self.box.maxsize(width=500, height=250)
+        self.box.title("Error")
+        self.box.rowconfigure(0, weight=1)
+        self.box.rowconfigure(1, minsize=60)
+        self.box.rowconfigure(2, minsize=20)
+        self.box.columnconfigure(0, weight=1)
+    def buttons(self):
+        self.button_accept = Button(master=self.box, text="Accept", sticky=None, row=1, column=0,
+                                    command=self.box.destroy)
+    def close_frame(self, time=8000):
+        self.after(time, lambda: self.box.destroy())
+    def text_box(self, message):
+        self.text_frame = customtkinter.CTkTextbox(master=self.box, wrap="word", state="normal", font=FONT_LABEL_ERROR,
+                                                   text_color=SELECTED_BLUE, border_spacing=10)
+        self.text_frame.grid(row=0, column=0, sticky="nsew")
+        self.text_frame.insert("0.0", f"{message}")
+        self.text_frame.configure(state="disabled")
 
 
 
